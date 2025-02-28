8c5bc3db254e6b35ac51cd340c41f6010d93638f
hassan-jamshaid10 <hjamshaid81@gmail.com>
2025-03-01 01:24:57 +0500

    CORS ENABLED index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const authRoutes = require("./Routes/authRoutes");
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -23,11 +7,14 @@ const pool = require("./Config/db");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-// Check database connection
 pool.connect()
   .then(() => console.log("Connected to the database"))
   .catch(err => console.error("Database connection error:", err));
